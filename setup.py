b219206dfa8d709a97480e7dff343b1828565380
firefirer1983 <fyman.zhang@gmail.com>
2021-07-23 09:10:14 +0800

    增添setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='flask-ground-up',
+    version='',
+    packages=[''],
+    url='https://github.com/firefirer1983/flask-ground-up',
+    license='',
+    author='xy',
+    author_email='fyman.zhang@gmail.com',
+    description='flask zero to hearo'
+)
